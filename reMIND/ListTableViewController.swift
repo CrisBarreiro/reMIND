a71fb5a0067d252b7b75e6d79cfcf2994b501f3d
Cristina S. Barreiro <cristina.sbarreiro@situm.es>
2017-12-15 20:21:00 +0100

    Add and display tasks

--- reMIND/ListTableViewController.swift
+++ reMIND/ListTableViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ListTableViewController: UITableViewController {
 
+    var taskManager = TaskManager.sharedInstance
     
     @IBAction func addTask(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "New Task", message: "Task Name", preferredStyle: .alert)
+        alertController.addTextField(configurationHandler: nil)
         
+        let alertAction = UIAlertAction(title: "Save", style: .default) { (alertAction) in
+            if let text = alertController.textFields?.first?.text {
+                self.taskManager.tasks.append(["title": text])
+                self.tableView.reloadData()
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(alertAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskManager.tasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let taskCell: TaskCell = tableView.dequeueReusableCell(withIdentifier: "CellIdentifier", for: indexPath) as! TaskCell
+        let task = taskManager.tasks[indexPath.row]
+        taskCell.lblTask.text = task["title"]
+        if let image = task["icon"] {
+            taskCell.imgTask.image = UIImage(named: image)
+        } else {
+            taskCell.imgTask.image = UIImage(named: "img_no_icon")
+        }
+        return taskCell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80.0
+    }
 }
