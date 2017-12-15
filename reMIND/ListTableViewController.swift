bdc28bb6b171a9c2e877a53162c1a5cfc4a1ace3
Cristina S. Barreiro <cristina.sbarreiro@situm.es>
2017-12-15 20:35:43 +0100

    reload data on viewWillAppear

--- reMIND/ListTableViewController.swift
+++ reMIND/ListTableViewController.swift
@@ -59,4 +59,9 @@ class ListTableViewController: UITableViewController {
         
         return [complete]
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
 }
